102a34ce2ccfd8b1451d5db5b2ec86cb14eaaa8b
Owiber <oliver@owiber.com>
2012-03-22 17:13:07 -0500

    fixing copyright/licensing note

--- numberformat.js
+++ numberformat.js
@@ -1,3 +1,4 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved
 // Copyright 2012 Oliver Wong. All Rights Reserved
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
